00ec4c7c991347b29c79c2f15cca23411672c3d9
TornaxO7 <tornax@proton.me>
2024-11-09 23:53:03 +0100

    removing boilerplate code

--- app/src/main/java/de/tornaxo7/pikado/MainActivity.kt
+++ app/src/main/java/de/tornaxo7/pikado/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PikadoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PikadoTheme {
-        Greeting("Android")
     }
 }
