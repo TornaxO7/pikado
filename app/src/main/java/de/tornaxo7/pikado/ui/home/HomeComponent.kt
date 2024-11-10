3c1dae4f5a52f3afe7418da1c6ef5bfad95ebb1a
TornaxO7 <tornax@proton.me>
2024-11-10 01:39:16 +0100

    home: implement top bar

--- app/src/main/java/de/tornaxo7/pikado/ui/home/HomeComponent.kt
+++ app/src/main/java/de/tornaxo7/pikado/ui/home/HomeComponent.kt
@@ -1,9 +1,20 @@
 package de.tornaxo7.pikado.ui.home
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import de.tornaxo7.pikado.R
+import de.tornaxo7.pikado.ui.theme.PikadoTheme
 
 @Composable
 fun HomeComponent(
@@ -12,9 +23,37 @@ fun HomeComponent(
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeComponentTopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.home_title),
+            )
+        },
+        actions = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(id = R.string.home_search)
+                )
+            }
 
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.filter),
+                    contentDescription = stringResource(
+                        id = R.string.home_filter
+                    )
+                )
+            }
+        },
+    )
 }
 
 @Composable
@@ -27,9 +66,11 @@ private fun HomeContent(
 @Preview(showBackground = true)
 @Composable
 private fun HomeContentPreview() {
-    Scaffold(
-        topBar = { HomeComponentTopBar()}
-    ){
-        HomeContent(it)
+    PikadoTheme {
+        Scaffold(
+            topBar = { HomeComponentTopBar() }
+        ) {
+            HomeContent(it)
+        }
     }
 }
